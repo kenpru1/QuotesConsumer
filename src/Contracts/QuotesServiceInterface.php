267cfec91d0945c5875b19e2ee59a257e4738bab
Kenneth Alvarez <kenpru1@gmail.com>
2025-04-06 10:12:45 -0400

    actualizacion

---
+++ src/Contracts/QuotesServiceInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Vendor\QuotesConsumer\Contracts;
+
+interface QuotesServiceInterface
+{
+    /**
+     * Obtener todas las citas
+     *
+     * @return array
+     */
+    public function getAllQuotes(): array;
+
+    /**
+     * Obtener una cita aleatoria
+     *
+     * @return array|null
+     */
+    public function getRandomQuote(): ?array;
+
+    /**
+     * Obtener una cita por ID
+     *
+     * @param int $id
+     * @return array|null
+     */
+    public function getQuoteById(int $id): ?array;
+}
